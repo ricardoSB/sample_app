4684e487759a1df341711277f754e30445e49e18
Juan Ruiz <invitado@perdidita.(none)>
2014-04-01 20:35:54 +0100

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,25 @@
 class User < ActiveRecord::Base
+
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+  def User.encrypt(token) # 4
+    Digest::SHA1.hexdigest(token.to_s) # SHA1 faster than bcrypt
+  end
+
+  private # hidden from everyone except the User model
+  def create_remember_token # 2
+    self.remember_token = User.encrypt(User.new_remember_token) # self = the object being created
+  end
+
 end
